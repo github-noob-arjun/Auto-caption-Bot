e5e07be508ea3feb023597e854a7fb2f1ecaf1af
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-08-07 22:56:24 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("APP_ID", 12345678))
+API_HASH = os.environ.get("API_HASH", "")
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in os.environ.get('AUTH_CHANNEL', '0').split()]
