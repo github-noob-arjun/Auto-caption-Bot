40e2335223e2364c02d66c3f2341a84198c1654d
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-09-06 08:51:37 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -60,7 +60,7 @@ async def caption(client, message: Message):
 
 @App.on_message(filters.media & filters.chat(AUTOFILTER_CHANNEL)) #filters.channel)
 async def caption(client, message: Message):
-    C,_ = message.caption.split("Uploaded")
+    C = message.caption.split("Uploaded")[0]
     if C:
         await message.edit_caption(f"**__{C}__**")
     else:
